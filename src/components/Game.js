2d7f599983e26c1f1941d5a863f1f5f3c421a883
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-10-16 00:22:05 +0300

    Improve Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Game = () => {
+  const [ gameState, setGameState ] = useState({});
+
+  useEffect(() => {
+    setGameState({
+      score: 0,
+    });
+  }, []);
+
+  const loveKukka = () => {
+    const newState = { ...gameState };
+    newState.score++;
+    setGameState(newState);
+  };
+
   return (
     <>
       <h2>Game</h2>
+      <p>Score: { gameState.score != null ? gameState.score : "Loading..." }</p>
+      <button onClick={ () => loveKukka() }>Rakasta kukkaasi</button>
     </>
   );
 };
