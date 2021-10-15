0aa3dcc97a351b54d68185a5377d5e9193e3b42c
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-10-15 19:39:01 +0300

    Change indent to 2 spaces

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.scss";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
 };
 
 export default App;
