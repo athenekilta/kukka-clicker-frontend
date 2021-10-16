7b3bb7d6551481ffa33ea8a29cffc6e30bff6ec0
Oliver Mulari <oliver@feelbie.com>
2021-10-16 14:17:33 +0300

    leaderboard styles

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,13 +2,20 @@ import React from "react";
 
 const Leaderboard = ({ leaderboard = [] })=> {
   return (
-    <ul>
-      {leaderboard?.map((user, i) => {
-        return (
-          <li key={user.username}>{i+1}. {user.username}: {user.score}</li>
-        );
-      })}
-    </ul>
+    <article className="my-2">
+      <h1 className="text-lg">Isoimmat kasvattajat</h1>
+      <ul>
+        {leaderboard?.map((user, i) => {
+          const medal = i === 0 ? "🥇" : i === 1 ? "🥈" : i === 2 ? "🥉" : null;
+          return (
+            <li
+              key={user.username}
+              className="p-2"
+            >{i+1}. <b>{medal} {user.username}</b>: {user.score}</li>
+          );
+        })}
+      </ul>
+    </article>
   );
 };
 
