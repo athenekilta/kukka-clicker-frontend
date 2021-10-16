a2fce2826f1438bd6ee708aa9d2ea7cc355e187e
Oliver Mulari <oliver@feelbie.com>
2021-10-17 02:20:05 +0300

    add quoutes

--- src/components/GameQuote.js
+++ src/components/GameQuote.js
@@ -1,17 +1,55 @@
 import React, { useMemo } from "react";
 
 const QUOTES = [
-  {value: 0, options: ["kasvata kukkaa klikkaamalla"] },
-  {value: 100, options: ["kukka alkaa kasvamaan"] },
-  {value: 200, options: ["onpa iso kukka"] },
-  {value: 420, options: ["blazing"] },
-  {value: 1000000000, options: ["ai saaataaaanaaaaa"] },
+  {
+    value: 0, 
+    description: "kasvata kukkaa klikkaamalla"
+  },
+  {
+    value: 1, 
+    description: "kukka alkaa nousta penkistä" 
+  },
+  {
+    value: 5, 
+    description: "onko se puu?"
+  },
+  {
+    value: 10, 
+    description: "mänty sen on pakko olla"
+  },
+  {
+    value: 200, 
+    description: "onpa iso kukka"
+  },
+  {
+    value: 420, 
+    description: "blazing" 
+  },
+  {
+    value: 1000, 
+    description: "tämähän on mukavaa ajanviihdettä" 
+  },
+  {
+    value: 2000, 
+    description: "viva la revolution!"
+  },
+  {
+    value: 10000, 
+    description: "mistä nämä ravinteet on ostettu?"
+  },
+  {
+    value: 100000, 
+    description: "onko se lintu, onko se lentokone?"
+  },
+  {
+    value: 1000000000, 
+    description: "ai saaataaaanaaaaa" 
+  },
 ].sort((l, r) => r.value - l.value);
 
 const GameQuote = ({ score = 0 }) => {
   const quote = useMemo(() => {
-    const options = QUOTES.find((q) => q.value <= score).options;
-    return options[Math.floor(Math.random() * options.length)];
+    return QUOTES.find((q) => q.value <= score).description;
   }, [score]);
 
   return <p className="text-base text-gray-700">&quot;{quote}&quot;</p>;
