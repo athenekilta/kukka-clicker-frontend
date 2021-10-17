be6b26020f0394438351702f9f2d9d869706296e
Oliver Mulari <oliver@feelbie.com>
2021-10-17 12:29:38 +0300

    fix error

--- src/components/StandaloneLeaderboard.js
+++ src/components/StandaloneLeaderboard.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect} from "react";
 import { io } from "socket.io-client";
 import { baseUrl } from "../services/authService";
 import Leaderboard from "./Leaderboard";
