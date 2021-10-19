b66ad1cf2572546d1ad3d0005be9456129adaaa9
Oliver Mulari <oliver@feelbie.com>
2021-10-19 11:15:51 +0300

    add quoutes

--- src/components/GameQuote.js
+++ src/components/GameQuote.js
@@ -34,20 +34,40 @@ const QUOTES = [
     description: "viva la revolution!"
   },
   {
-    value: 10000, 
+    value: 1e4, 
     description: "mistä nämä ravinteet on ostettu?"
   },
   {
-    value: 100000, 
+    value: 1e5, 
     description: "onko se lintu, onko se lentokone?"
   },
   {
-    value: 10000000,
+    value: 1e7,
     description: "nu kör vi!"
   },
   {
-    value: 10000000000, 
+    value: 1e9, 
+    description: "tämä planeetta on liian pieni kukan tarpeisiin" 
+  },
+  {
+    value: 1e10, 
     description: "ai saaataaaanaaaaa" 
+  },
+  {
+    value: 1e25, 
+    description: "se on kaikkialla!!" 
+  },
+  {
+    value: 1e27, 
+    description: "mihin kasvaminen loppuu, täytyyhän sen joskus loppua" 
+  },
+  {
+    value: 1e30, 
+    description: "we are in the endgame now" 
+  },
+  {
+    value: 1e37, 
+    description: "nyt voidaan turvallisesti todeta että aika suureksi on kukka kasvanut" 
   },
 ].sort((l, r) => r.value - l.value);
 
